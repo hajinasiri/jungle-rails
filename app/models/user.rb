bdefd1a6d0f279dc69f98010aac45d54b0e7cd36
hajinasiri <hajinasiri@gmail.com>
2017-08-31 20:26:19 +0000

    users can login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true
   validates :password, presence: true
+  before_save { self.email = email.downcase }
+  has_secure_password
 end
